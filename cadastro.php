0a3372bfcb1dd71cfc50482cd3ef11e068fb3b0a
willalves <william@planner.net.br>
2016-02-24 14:34:54 -0300

    alterando o arquivo cadastro para inserir usuarios

--- cadastro.php
+++ cadastro.php
@@ -1,32 +1,29 @@
 <?php
-	include_once("conexao/conecta.php");
-
-	if(@$_GET['go'] == 'cadastrar'){
-		$nome = $_POST['nome'];
-		$email = $_POST['email'];
-		$user = $_POST['usuario'];
-		$pwd = $_POST['senha'];
-
-		if(empty($nome)){
-			echo "All fields must be filled!";
-		}elseif(empty($email)){
-			echo "All fields must be filled!";
-		}elseif(empty($user)){
-			echo "All fields must be filled!";
-		}elseif(empty($pwd)){
-			echo "All fields must be filled!";
-		}else{
-			$query1 = mysql_num_rows(@mysql_query("SELECT * FROM login WHERE login = '$user'"));
-			if($query1 == 1){
-				echo "This user already exists.";
-			}else{
-				@mysql_query("isert into login (nome, email, usuario, senha) values ('$nome','$email','$user','$pwd')");
-				echo "User successfully registered!";
-				header("Location: index.phtml")
-			}
-		}
+session_start();
+	include("conexao/conecta.php");
+	 
+	if(isset($_REQUEST["post_back"])){
+	 
+	    if(empty($_POST["nome"]) || empty($_POST["email"]) || empty($_POST["usuario"]) || empty($_POST["senha"])){
+	   
+	        echo "<script>alert(‘Preencha todos os campos antes de enviar seu cadastro!’);</script>";  
+	   
+	    }else{
+	        $tb = $conn->prepare("insert into login(nm_login, email_login, usuario_login, senha_login) values(:nome, :email, :usuario, :senha)");
+	        $tb->bindParam(":nome", $_POST["nome"], PDO::PARAM_STR);
+	        $tb->bindParam(":email", $_POST["email"], PDO::PARAM_STR);
+	        $tb->bindParam(":usuario", $_POST["usuario"], PDO::PARAM_STR);
+	        $tb->bindParam(":senha", $_POST["senha"], PDO::PARAM_STR);
+	        $tb->execute();
+	        $tb = null;
+	       
+	        echo "Cadastro feito com sucesso.";
+	       
+	    }
+	 
 	}
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
@@ -39,10 +36,10 @@
 			<div id="div-meio-inserir">
 				<h2>Creating new account</h2>
 				<p class="criar-conta">Enter the information below</p>
-				<form method="POST" action="?go=cadastrar">
-					<label class="cadastra-senha">Nome: </label><input type="text" name="nome" id="cadastra-nome"><br>
-					<label class="cadastra-senha">Email: </label><input type="text" name="email" id="cadastra-email"><br>
-					<label class="cadastra-email">Usuario: </label><input type="text" name="usuario" id="cadastra-usuario"><br>
+				<form method="post" action="cadastro.php">
+					<label class="cadastra-nome">Nome: </label><input type="text" name="nome" id="cadastra-nome"><br>
+					<label class="cadastra-email">Email: </label><input type="text" name="email" id="cadastra-email"><br>
+					<label class="cadastra-usuario">Usuario: </label><input type="text" name="usuario" id="cadastra-usuario"><br>
 					<label class="cadastra-senha">Senha: </label><input type="password" name="senha" id="cadastra-senha"><br>
 					<input type="submit" value="Creating Account" id="botao-cadastrar" name="cadastrar">
 				</form>
